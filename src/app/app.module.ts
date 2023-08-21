69e9196a8e19580bbe86a3fe3875c045a148a9cc
Pierre <pierre.marcel.zimermmann@gmail.com>
2023-08-21 11:55:08 +0200

    Remove calendar unused

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarComponent } from './calendar/calendar.component';
 import { EventUserInputComponent } from './event-user-input/event-user-input.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ListDisplayComponent } from './list-display/list-display.component'; 
@@ -32,7 +31,6 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 @NgModule({
   declarations: [
     AppComponent,
-    CalendarComponent,
     EventUserInputComponent,
     ListDisplayComponent,
     EventEditComponent,
