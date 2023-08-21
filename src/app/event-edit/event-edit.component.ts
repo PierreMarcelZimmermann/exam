e79551cb778300d7a323edecd7d83b1c02b23998
Pierre <pierre.marcel.zimermmann@gmail.com>
2023-08-21 09:30:55 +0200

    Fix edit component

--- src/app/event-edit/event-edit.component.ts
+++ src/app/event-edit/event-edit.component.ts
@@ -1,66 +1,137 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DataService } from '../data.service';
-import { Event } from '../event.interface';
-import { Category } from '../category.interface';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
 import { CategoryService } from '../category.service';
+import { Observable } from 'rxjs';
+import { Category } from '../category.interface';
+import { Event } from '../event.interface';
 
 @Component({
   selector: 'app-event-edit',
   templateUrl: './event-edit.component.html',
   styleUrls: ['./event-edit.component.css']
 })
-export class EventEditComponent {
-  event$!: Observable<Event>;
+export class EventEditComponent implements OnInit {
+  eventForm: FormGroup;
   allCategories: Category[] = [];
-  selectedCategoryIds: number[] = [];
 
   constructor(
     private categoryService: CategoryService,
     private route: ActivatedRoute,
     private router: Router,
+    private fb: FormBuilder,
     private dataService: DataService
   ) {
-    // Fetch event based on route parameter
-    this.event$ = this.route.params.pipe(
-      switchMap(params => this.dataService.getEventByID(+params['id']))
-    );
+    this.eventForm = this.fb.group({
+      id: [null],
+      title: ['', Validators.required],
+      location: [''],
+      organizer: ['', Validators.required],
+      startDate: ['', Validators.required],
+      startTime: ['', Validators.required],
+      endDate: ['', Validators.required],
+      endTime: ['', Validators.required],
+      status: ['Free', Validators.required],
+      allday: [false],
+      webpage: [''],
+      categories: [[], Validators.required]
+    });
 
-    // Load all categories for the category selection
+    this.route.params.subscribe(params => {
+      const eventId = +params['id'];
+      this.loadEvent(eventId);
+    });
+
     this.loadAllCategories();
   }
 
-  /**
-   * Loads all categories from the category service.
-   */
+  ngOnInit() {}
+
+  loadEvent(eventId: number) {
+    this.dataService.getEventByID(eventId).subscribe(event => {
+      // Populate the form controls with event data
+      this.eventForm.patchValue(event);
+  
+      // Set selected category IDs
+      const selectedCategoryIds = event.categories.map(category => category.id);
+      this.eventForm.get('categories')?.patchValue(selectedCategoryIds);
+  
+      // Set the start date and time in the form
+      const startDate = new Date(event.start);
+      const startTime = this.getTimeString(startDate);
+      this.eventForm.get('startDate')?.setValue(startDate);
+      this.eventForm.get('startTime')?.setValue(startTime);
+  
+      // Set the end date and time in the form
+      const endDate = new Date(event.end);
+      const endTime = this.getTimeString(endDate);
+      this.eventForm.get('endDate')?.setValue(endDate);
+      this.eventForm.get('endTime')?.setValue(endTime);
+    });}
+  
+  
+  private getTimeString(date: Date): string {
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
+
   loadAllCategories() {
     this.categoryService.getAllCategories().subscribe(categories => {
       this.allCategories = categories;
     });
   }
 
-  /**
-   * Updates the event with new data and navigates back to the events list.
-   * @param event The event with updated data.
-   */
-  updateEvent(event: Event) {
-    event.categories = this.selectedCategoryIds.map(id => this.allCategories.find(category => category.id === id)!);
-    this.dataService.updateEvent(event.id, event).subscribe(() => {
-      this.router.navigate(['/events']);
-    });
-  }
+  updateEvent() {
+    if (this.eventForm.valid) {
+      const updatedEvent: Event = this.eventForm.value;
 
-  /**
-   * Toggles the selection of a category.
-   * @param categoryId The ID of the category to toggle.
-   */
-  toggleCategory(categoryId: number) {
-    if (this.selectedCategoryIds.includes(categoryId)) {
-      this.selectedCategoryIds = this.selectedCategoryIds.filter(id => id !== categoryId);
-    } else {
-      this.selectedCategoryIds.push(categoryId);
+      this.dataService.updateEvent(updatedEvent.id, updatedEvent).subscribe(() => {
+        this.router.navigate(['/events']);
+      });
     }
   }
+
+  private formatDateTime(date: Date, time: string, isAllDay: boolean): string {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    const [rawHours, minutes, period] = time.split(/:| /);
+    const hours = parseInt(rawHours) + (period === 'PM' ? 12 : 0);
+    const formattedHours = String(hours).padStart(2, '0');
+    const formattedMinutes = String(minutes).padStart(2, '0');
+
+    return `${year}-${month}-${day}T${formattedHours}:${formattedMinutes}`;
+  }
+
+  onSubmit() {
+    if (this.eventForm.valid) {
+  
+      const isAllDay = this.eventForm.value.allday;
+      const startDate = this.eventForm.value.startDate;
+      const endDate = this.eventForm.value.endDate;
+  
+      const startDateTime = this.formatDateTime(startDate, this.eventForm.value.startTime, isAllDay);
+      const endDateTime = this.formatDateTime(endDate, this.eventForm.value.endTime, isAllDay);
+  
+      console.log('startDateTime:', startDateTime);
+      console.log('endDateTime:', endDateTime);
+  
+      const updatedEvent: Event = {
+        ...this.eventForm.value,
+        start: startDateTime,
+        end: endDateTime,
+        categories: this.eventForm.value.categories.map((categoryId: number) => ({ id: categoryId })),
+      };
+  
+      console.log('updatedEvent:', updatedEvent);
+  
+      this.dataService.updateEvent(updatedEvent.id, updatedEvent).subscribe(() => {
+        this.router.navigate(['/events']);
+      });
+    }
+  }
+  
 }
